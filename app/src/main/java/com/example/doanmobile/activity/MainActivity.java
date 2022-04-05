2e5b84ce0ef84674ccb6ca73e20a297bd53ef380
thuylinhnguyenjy <79857091+thuylinhnguyenjy@users.noreply.github.com>
2022-04-05 15:33:03 +0700

    add bottom nav

--- app/src/main/java/com/example/doanmobile/activity/MainActivity.java
+++ app/src/main/java/com/example/doanmobile/activity/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.doanmobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.doanmobile.R;
+import com.example.doanmobile.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        bottomNavChangeFragment();
     }
+
+    protected void bottomNavChangeFragment(){
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_bottom_nav_container);
+        final NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNav, navController);
+    }
+
 }
